e882216b8c96b3afdf92d69d36b934a57342e99d
nishasinghgit <101825335+nishasinghgit@users.noreply.github.com>
2022-03-17 23:34:41 +0530

    Add files via upload

---
+++ 2-arthmetic.c
@@ -0,0 +1,18 @@
+#include<stdio.h>      //variable
+
+int main(){
+int a = 5;
+//  int b = 4.5;  //not recommended because 8.5 is not an integer
+float b = 4.5;
+char c = 'v';
+int d = 50;
+int e = 50 + 5;
+printf("the value of a is %d \n" ,a);
+printf("the value of b is %d \n",b);
+printf("the value of c is %d \n",c);
+printf("the value of d is %d \n",a+d);
+printf("the value of e is %d \n",e);
+return 0;
+}
+
+
